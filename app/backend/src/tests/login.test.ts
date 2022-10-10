d29dd3ac85a2399f50dceb6b18ff19f1255351bb
Leo <leonardoandrei41@gmail.com>
2022-10-10 15:43:45 -0300

    test: cannot log in without email or password

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -16,5 +16,19 @@ describe('/login endpoint', () => {
       expect(response).to.have.status(200);
       expect(response.body).to.have.property('token');
     });
+
+    it('cannot log in without email', async () => {
+      const response = await chai.request(app).post('/login').send({ password: login.password });
+
+      expect(response).to.have.status(400);
+      expect(response.body.message).to.be.eq('All fields must be filled');
+    });
+
+    it('cannot log in without password', async () => {
+      const response = await chai.request(app).post('/login').send({ email: login.email });
+
+      expect(response).to.have.status(400);
+      expect(response.body.message).to.be.eq('All fields must be filled');
+    });
   });
 });
