b2efaa413eb77e83febb00314fbdb0fc9e9ae4b8
Leo <leonardoandrei41@gmail.com>
2022-10-10 16:54:27 -0300

    feat: mock - token and invalid token

--- app/backend/src/mocks/login.ts
+++ app/backend/src/mocks/login.ts
@@ -7,3 +7,8 @@ export const invalidLogin = {
   email: 'admin@.com',
   password: 'secretadmin',
 };
+
+export const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiw'
+  + 'iaWF0IjoxNjY1NDI5NTU4LCJleHAiOjE2NjYwMzQzNTh9.x_iMaDwX8o89Q_DmVlSGL0l_1BDwZc9TS7_GwI872OA';
+
+export const invalidToken = 'asOt7SdPg6eDdge89reBogj53R3dco53';
