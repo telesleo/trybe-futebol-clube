2b694b103ed7506d4ddfe381afaeb9b3db2f906a
Leo <leonardoandrei41@gmail.com>
2022-10-11 11:07:56 -0300

    feat: endpoint GET /teams/:id

--- app/backend/src/routes/teams.routes.ts
+++ app/backend/src/routes/teams.routes.ts
@@ -7,6 +7,8 @@ const teams = Router();
 
 const teamController = new TeamController(new TeamService(Team));
 
+teams.get('/:id', (req: Request, res: Response, next: NextFunction) =>
+  teamController.getById(req, res, next));
 teams.get('/', (req: Request, res: Response, next: NextFunction) =>
   teamController.getAll(req, res, next));
 
