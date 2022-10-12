32724b281069059a63763ee3fd5838f84a249e60
Leo <leonardoandrei41@gmail.com>
2022-10-12 20:09:13 -0300

    feat: must have a valid token to create a match

--- app/backend/src/routes/matches.routes.ts
+++ app/backend/src/routes/matches.routes.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response, NextFunction } from 'express';
 import MatchController from '../controllers/match.controller';
 import MatchService from '../services/match.service';
 import Match from '../database/models/Match';
+import validateToken from '../middlewares/validateToken';
 
 const matches = Router();
 
@@ -9,7 +10,7 @@ const matchController = new MatchController(new MatchService(Match));
 
 matches.get('/', (req: Request, res: Response, next: NextFunction) =>
   matchController.getAll(req, res, next));
-matches.post('/', (req: Request, res: Response, next: NextFunction) =>
+matches.post('/', validateToken, (req: Request, res: Response, next: NextFunction) =>
   matchController.create(req, res, next));
 matches.patch('/:id/finish', (req: Request, res: Response, next: NextFunction) =>
   matchController.upgrateProgress(req, res, next));
