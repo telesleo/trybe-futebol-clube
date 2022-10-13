2dedaa599b170566a082e6e8bc52525935862ea7
Leo <leonardoandrei41@gmail.com>
2022-10-12 23:01:32 -0300

    feat: add mock matches

---
+++ app/backend/src/mocks/matches.ts
@@ -0,0 +1,82 @@
+export default [
+  {
+    homeTeam: 16,
+    homeTeamGoals: 1,
+    awayTeam: 8,
+    awayTeamGoals: 1,
+    inProgress: false,
+  },
+  {
+    homeTeam: 9,
+    homeTeamGoals: 1,
+    awayTeam: 14,
+    awayTeamGoals: 1,
+    inProgress: false,
+  },
+  {
+    homeTeam: 4,
+    homeTeamGoals: 3,
+    awayTeam: 11,
+    awayTeamGoals: 0,
+    inProgress: false,
+  },
+  {
+    homeTeam: 3,
+    homeTeamGoals: 0,
+    awayTeam: 2,
+    awayTeamGoals: 0,
+    inProgress: false,
+  },
+  {
+    homeTeam: 7,
+    homeTeamGoals: 1,
+    awayTeam: 10,
+    awayTeamGoals: 1,
+    inProgress: false,
+  },
+  {
+    homeTeam: 5,
+    homeTeamGoals: 1,
+    awayTeam: 13,
+    awayTeamGoals: 1,
+    inProgress: false,
+  },
+  {
+    homeTeam: 4,
+    homeTeamGoals: 1,
+    awayTeam: 12,
+    awayTeamGoals: 1,
+    inProgress: true,
+  },
+  {
+    homeTeam: 8,
+    homeTeamGoals: 1,
+    awayTeam: 14,
+    awayTeamGoals: 2,
+    inProgress: true,
+  },
+  {
+    homeTeam: 13,
+    homeTeamGoals: 1,
+    awayTeam: 2,
+    awayTeamGoals: 1,
+    inProgress: true,
+  },
+];
+
+export const match = {
+  homeTeam: 8,
+  awayTeam: 7,
+  homeTeamGoals: 2,
+  awayTeamGoals: 2,
+  inProgress: true,
+};
+
+export const createdMatch = {
+  id: 9,
+  homeTeam: 8,
+  awayTeam: 7,
+  homeTeamGoals: 2,
+  awayTeamGoals: 2,
+  inProgress: true,
+};
