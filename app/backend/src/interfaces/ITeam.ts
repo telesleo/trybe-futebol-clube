e465b6abe7fd301fa7c9bec8af4084a33f725d8c
Leo <leonardoandrei41@gmail.com>
2022-10-12 23:00:53 -0300

    feat: add interface Iteam

--- app/backend/src/interfaces/ITeam.ts
+++ app/backend/src/interfaces/ITeam.ts
@@ -2,3 +2,16 @@ export default interface ITeam {
   id?: number;
   teamName: string;
 }
+
+export interface ILeaderboardTeam {
+  name: string;
+  totalPoints: number;
+  totalGames: number;
+  totalVictories: number;
+  totalDraws: number;
+  totalLosses: number;
+  goalsFavor: number;
+  goalsOwn: number;
+  goalsBalance: number;
+  efficiency: number;
+}
